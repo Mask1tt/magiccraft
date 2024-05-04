7fb0c0482d3d977219856e532adbbcd5fed67e82
mask1tt <mask1tt@mail.ru>
2024-05-04 12:19:02 +0300

    Добавлен: блок невидимости, новые растения.

--- src/main/java/net/bambuki/magiccraft/block/ModBlocks.java
+++ src/main/java/net/bambuki/magiccraft/block/ModBlocks.java
@@ -1,6 +1,9 @@
 package net.bambuki.magiccraft.block;
 
 import net.bambuki.magiccraft.MagicCraft;
+import net.bambuki.magiccraft.block.custom.InvisibilityBlock;
+import net.bambuki.magiccraft.block.custom.MandragoraCropBlock;
+import net.bambuki.magiccraft.block.custom.TinLampBlock;
 import net.bambuki.magiccraft.item.ModCreativeModeTab;
 import net.bambuki.magiccraft.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -8,6 +11,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -40,6 +44,15 @@ public class ModBlocks {
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(3, 7)), ModCreativeModeTab.MAGICCRAFT_TAB);
+    public static final RegistryObject<Block> INVISIBILITY_BLOCK = registerBlock("invisibility_block",
+            () -> new InvisibilityBlock(BlockBehaviour.Properties.of(Material.WOOD)
+                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.MAGICCRAFT_TAB);
+    public static final RegistryObject<Block> TIN_LAMP = registerBlock("tin_lamp",
+            () -> new TinLampBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()
+                    .lightLevel(state -> state.getValue(TinLampBlock.LIT) ? 15 : 0)), ModCreativeModeTab.MAGICCRAFT_TAB);
+    public static final RegistryObject<Block> MANDRAGORA_CROP = BLOCKS.register("mandragora_crop",
+            () -> new MandragoraCropBlock(BlockBehaviour.Properties.copy(Blocks.WHEAT)));
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
